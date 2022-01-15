7d376905cf0f5fa6dbcb7809eefb412c1758c4b2
Florian Spiess <florian.spiess@unibas.ch>
2022-01-15 19:35:30 +0100

    Updated feed template and style sheet.

--- plate/feed.py
+++ plate/feed.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template
 
 from plate.articles import retrieve_articles
@@ -8,4 +10,11 @@ bp = Blueprint('feed', __name__)
 @bp.route('/')
 def get_feed():
     articles = retrieve_articles('all', 32)
+    for article in articles:
+        published = datetime.fromtimestamp(article['published'])
+        article['published'] = published.strftime('%d.%m.%Y %H:%M')
+        summary = article['summary']
+        max_length = 500
+        if len(summary) > max_length:
+            article['summary'] = summary[:max_length] + '...'
     return render_template('base.html', articles=articles)
