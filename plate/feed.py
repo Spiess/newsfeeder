a6602080d09f1b96caf9e31af46688ffc7130f12
Florian Spiess <florian.spiess@unibas.ch>
2022-01-17 21:36:47 +0100

    Updated main page with flexbox instead of columns.

--- plate/feed.py
+++ plate/feed.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 from flask import Blueprint, render_template
@@ -7,12 +8,40 @@ from plate.articles import retrieve_articles
 bp = Blueprint('feed', __name__)
 
 
+def format_time_delta(delta):
+    """Formats time delta in seconds."""
+    spm = 60
+    sph = spm * 60
+    spd = sph * 24
+
+    seconds = int(delta % spm)
+    minutes = int((delta % sph) / spm)
+    hours = int((delta % spd) / sph)
+    days = int(delta / spd)
+
+    if delta < spm:
+        return f'{seconds} second{"s" if seconds != 1 else ""}'
+    elif delta < sph:
+        return f'{minutes} minute{"s" if minutes != 1 else ""}'
+    elif delta < spd:
+        return f'{hours} hour{"s" if hours != 1 else ""}, {minutes} minute{"s" if minutes != 1 else ""}'
+    else:
+        return f'{days} day{"s" if days != 1 else ""}, {hours} hour{"s" if hours != 1 else ""}, ' \
+               f'{minutes} minute{"s" if minutes != 1 else ""}'
+
+
 @bp.route('/')
-def get_feed():
-    articles = retrieve_articles('all', 32)
+@bp.route("/<sites>")
+@bp.route("/<sites>/<int:n_articles>")
+@bp.route("/<sites>/<int:n_articles>/<int:last_article_published>")
+def get_feed(sites='all', n_articles=32, last_article_published=None):
+    articles = retrieve_articles(sites, n_articles, last_article_published)
+    current_time = time.time()
     for article in articles:
+        pub_time = article['published']
+        article['published_since'] = format_time_delta(current_time - pub_time)
         published = datetime.fromtimestamp(article['published'])
-        article['published'] = published.strftime('%d.%m.%Y %H:%M')
+        article['published'] = published.strftime('%d.%m.%Y %H:%M (UTC)')
         summary = article['summary']
         max_length = 500
         if len(summary) > max_length:
